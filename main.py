9a0d2e40431c5b6fb1e76936cdb194a6a085a0e8
DOS <kiler027@gmail.com>
2021-03-30 21:56:27 +0300

    ...

--- main.py
+++ main.py
@@ -10,6 +10,10 @@ def func_from_str(f, x):
     return eval(f)
 
 
+def ones(x):
+    return (x ** 0).reshape(-1, 1)
+
+
 x = np.linspace(10 ** -10, 2 * np.pi, 1000)
 y = 100 * np.sin(x) + 0.5 * np.exp(x) + 300
 err = norm_dist(0, 100, 1000)
@@ -26,14 +30,14 @@ models_list = []
 
 for i in range(1, 4):
     for comb in combinations(range(len(functions)), i):
-        phi = np.power(x_train,0).reshape(-1, 1)
+        phi = ones(x_train)
         for k in comb:
             phi = np.concatenate((phi, func_from_str(functions[k], x_train.reshape(-1, 1))), axis=1)
         w = np.linalg.inv(np.dot(phi.T, phi)).dot(phi.T).dot(y_train)
         new_y_train = (w * phi).sum(axis=1)
         rmse_train = RMSE(y_train, new_y_train)
 
-        phi = np.power(x_valid, 0).reshape(-1, 1)
+        phi = ones(x_valid)
         for k in comb:
             phi = np.concatenate((phi, func_from_str(functions[k], x_valid.reshape(-1, 1))), axis=1)
         new_y_valid = (w * phi).sum(axis=1)
@@ -46,23 +50,20 @@ for i in range(1, 4):
         model['rmse_valid'] = rmse_valid
         models_list.append(model)
 models_list = sorted(models_list, key=lambda x: x['rmse_valid'])
-print(models_list[0])
 
-comb=models_list[0]['model']
-w=models_list[0]['w']
-phi = np.power(x_test, 0).reshape(-1, 1)
+comb = models_list[0]['model']
+w = models_list[0]['w']
+phi = ones(x_test)
 for k in comb:
     phi = np.concatenate((phi, func_from_str(k, x_test.reshape(-1, 1))), axis=1)
 new_y_test = (w * phi).sum(axis=1)
 rmse_test = RMSE(y_test, new_y_test)
 
-model={}
+model = {}
 model['model'] = comb
 model['w'] = w
-model['rmse_train']=models_list[0]['rmse_train']
+model['rmse_train'] = models_list[0]['rmse_train']
 model['rmse_test'] = rmse_test
 
-
-
-bar_plot(models_list,bin_count=3)
-bar_plot([model], bin_count=1, size=(6, 4), rmse_list=['rmse_train','rmse_test'], text=['обучающей','тестовой'])
+bar_plot(models_list, bin_count=3)
+bar_plot([model], bin_count=1, size=(6, 4), rmse_list=['rmse_train', 'rmse_test'], text=['обучающей', 'тестовой'])
